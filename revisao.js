28fdd315339c6ca505c6735cbad7ed56a9d4df1b
Guilherme-Engelmann <guilherme_engelmann@estudante.sesisenai.org.br>
2025-04-04 09:32:58 -0300

    first commit

---
+++ revisao.js
@@ -0,0 +1,35 @@
+/*
+console.log('Somador de Números');
+let somaTotal = 0;
+let quantidade = prompt('Digite quantos números deseja escrever');
+for (let i = 0; i < quantidade; i++) {  
+    let numero = parseFloat(prompt(`Digite o número ${i + 1}:`));  
+    somaTotal += numero; 
+}  
+console.log(somaTotal);
+*/
+/*
+console.log('Contagem regresiva');
+let contagem = 10; 
+
+const contagemRegressiva = setInterval (() =>{
+    if(contagem >0){
+        console.log(contagem);
+        contagem--;
+    }else{
+        clearInterval(contagemRegressiva);
+        console.log('Lançamento');
+    }
+
+}, 1000);
+*/
+console.log('Balço financeiro Anual');
+
+for( let i=0; i=12; i++){
+    let ganhoBruto = prompt('Digite o hanho Bruto da empresa');
+    let gastosMensais = prompt('Digite os gastos mensais da sua empresa');
+
+
+}
+    
+
